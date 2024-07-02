19e07c509d879f0c1a68d407882f41e0180bd387
ocavue <ocavue@gmail.com>
2024-07-03 04:58:07 +1000

    feat: support typescript 5.5

--- src/basic.js
+++ src/basic.js
@@ -1,18 +1,18 @@
 // @ts-check
 
 import { antfu } from './antfu.js'
-import { ignores } from './ignores.js'
 import { imports } from './imports.js'
 import { noOnlyTests } from './no-only-tests.js'
 import { packageJson } from './package-json.js'
 import { prettier } from './prettier.js'
+import { GLOB_EXCLUDE } from './shared.js'
 import { typescript } from './typescript.js'
 import { unicorn } from './unicorn.js'
 
 export function basic() {
   /** @type {import('eslint').Linter.FlatConfig[]} */
   const config = [
-    ...ignores(),
+    { ignores: [...GLOB_EXCLUDE] },
     ...typescript(),
     ...imports(),
     ...packageJson(),
