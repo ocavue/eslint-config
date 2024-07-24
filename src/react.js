2c3d75e670ca9c8c8103ccc8a0805dd40a97f059
ocavue <ocavue@gmail.com>
2024-07-24 15:57:13 +1000

    feat: update eslint-plugin-react

--- src/react.js
+++ src/react.js
@@ -7,34 +7,30 @@ import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
 import { GLOB_TS, GLOB_TSX } from './shared.js'
 
+/** @type {import('eslint').Linter.FlatConfig} */
+const reactRecommended = reactPlugin.configs.flat.recommended
+
 export function react() {
   /** @type {import('eslint').Linter.FlatConfig[]} */
   const config = [
     {
+      ...reactRecommended,
+      name: 'react',
       files: [GLOB_TS, GLOB_TSX],
-      plugins: {
-        react: reactPlugin,
-      },
-      languageOptions: {
-        parserOptions: {
-          ecmaFeatures: {
-            jsx: true,
-          },
-        },
-      },
       settings: {
         react: {
           version: 'detect',
         },
       },
       rules: {
-        ...reactPlugin.configs.recommended.rules,
+        ...reactRecommended.rules,
         'react/prop-types': 'off',
         'react/react-in-jsx-scope': 'off',
       },
     },
 
     {
+      name: 'react-hooks',
       files: [GLOB_TS, GLOB_TSX],
       plugins: {
         'react-hooks': reactHooksPlugin,
