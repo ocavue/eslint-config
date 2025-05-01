6d4405c730ba3034556b7a17e3f1989b2a6023e8
ocavue <ocavue@gmail.com>
2025-05-01 11:49:53 +1000

    feat: add rule `@typescript-eslint/consistent-type-imports`

--- src/typescript.js
+++ src/typescript.js
@@ -45,6 +45,7 @@ export function typescript() {
             fixStyle: 'inline-type-imports',
           },
         ],
+        '@typescript-eslint/no-import-type-side-effects': 'warn',
         '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
         '@typescript-eslint/no-unnecessary-parameter-property-assignment':
           'warn',
