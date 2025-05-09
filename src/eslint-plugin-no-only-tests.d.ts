690d255aa6f1da007284dd4a67e58c95a1420d53
ocavue <ocavue@gmail.com>
2025-05-09 12:49:32 +1000

    fix

--- src/eslint-plugin-no-only-tests.d.ts
+++ src/eslint-plugin-no-only-tests.d.ts
@@ -1,6 +1,6 @@
-import type { ESLint } from 'eslint'
-
 declare module 'eslint-plugin-no-only-tests' {
+  import type { ESLint } from 'eslint'
+
   const plugin: ESLint.Plugin
-  export default plugin
+  export = plugin
 }
