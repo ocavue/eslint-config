837e62200f78c7ae757d6db5257c787767d22528
ocavue <ocavue@gmail.com>
2025-05-19 16:47:42 +1000

    fix

--- src/typescript-v2.ts
+++ src/typescript-v2.ts
@@ -60,6 +60,27 @@ function recommendedTypeCheckedOnlyRules(): Rules {
 
   return {
     ...rules,
+
+    '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+
+    '@typescript-eslint/no-unsafe-call': 'warn',
+
+    '@typescript-eslint/no-unsafe-return': 'warn',
+
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+
+    '@typescript-eslint/no-floating-promises': 'warn',
+
+    '@typescript-eslint/restrict-template-expressions': 'off',
+
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: false },
+    ],
   }
 }
 
@@ -101,6 +122,18 @@ function tsOnlyRules(): Rules {
   return {
     ...recommendedTypeCheckedOnlyRules(),
     ...stylisticRules(),
+
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        // Allow type imports in type annotations (e.g. `type T = import('Foo').Foo`)
+        disallowTypeAnnotations: false,
+      },
+    ],
+
+    '@typescript-eslint/no-import-type-side-effects': 'warn',
+
+    '@typescript-eslint/no-unnecessary-parameter-property-assignment': 'warn',
   }
 }
 
--- src/typescript.ts
+++ src/typescript.ts
@@ -41,36 +41,13 @@ export function typescript(): Linter.Config[] {
         ...rules,
 
     
-        '@typescript-eslint/consistent-type-imports': [
-          'warn',
-          {
-            // Allow type imports in type annotations (e.g. `type T = import('Foo').Foo`)
-            disallowTypeAnnotations: false,
-          },
-        ],
-        '@typescript-eslint/no-import-type-side-effects': 'warn',
-        '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
-        '@typescript-eslint/no-unnecessary-parameter-property-assignment':
-          'warn',
-        '@typescript-eslint/restrict-plus-operands': 'warn',
-        '@typescript-eslint/no-unsafe-call': 'warn',
-        '@typescript-eslint/no-unsafe-return': 'warn',
-        '@typescript-eslint/no-unsafe-argument': 'warn',
-        '@typescript-eslint/no-unsafe-member-access': 'warn',
-        '@typescript-eslint/no-unsafe-assignment': 'warn',
-        '@typescript-eslint/no-floating-promises': 'warn',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/restrict-template-expressions': 'off',
 
-        '@typescript-eslint/no-extra-semi': 'off',
-        // '@typescript-eslint/prefer-function-type': 'warn',
+    
+     
+   
 
-        '@typescript-eslint/no-misused-promises': [
-          'error',
-          { checksVoidReturn: false },
-        ],
+
+
         '@typescript-eslint/await-thenable': 'error',
         '@typescript-eslint/unbound-method': 'error',
 
