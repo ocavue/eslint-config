c57cd144d6d649851e24d03e9da59c157ed8a207
ocavue <ocavue@gmail.com>
2025-05-19 16:51:46 +1000

    format

--- src/basic.ts
+++ src/basic.ts
@@ -1,6 +1,7 @@
 import type { Linter } from 'eslint'
 
 import { antfu } from './antfu.js'
+import { gitignore } from './gitignore.js'
 import { ignores } from './ignores.js'
 import { imports } from './imports.js'
 import { noOnlyTests } from './no-only-tests.js'
@@ -8,7 +9,6 @@ import { packageJson } from './package-json.js'
 import { prettier } from './prettier.js'
 import { typescript } from './typescript.js'
 import { unicorn } from './unicorn.js'
-import { gitignore } from './gitignore.js'
 
 export function basic(): Linter.Config[] {
   return [
