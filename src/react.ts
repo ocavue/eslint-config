3b1014ec3060face88ce377ea34636fccb1bee59
ocavue <ocavue@gmail.com>
2025-12-09 22:58:22 +1100

    feat: migrate to `@eslint-react/eslint-plugin`

--- src/react.ts
+++ src/react.ts
@@ -1,13 +1,13 @@
-import reactPlugin from 'eslint-plugin-react'
+import eslintReact from '@eslint-react/eslint-plugin'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
 import { resolveReactOptions, type ReactOptions } from './options.js'
 import type { Config } from './types.js'
 
 export function react(options?: ReactOptions): Config[] {
-  const { files, reactCompiler, version } = resolveReactOptions(options)
+  const { files, reactCompiler } = resolveReactOptions(options)
 
-  const reactRecommended: Config = reactPlugin.configs.flat.recommended
+  const reactRecommended: Config = eslintReact.configs['recommended-typescript']
 
   const reactHooksRecommended: Config =
     reactHooksPlugin.configs.flat['recommended']
@@ -19,16 +19,6 @@ export function react(options?: ReactOptions): Config[] {
       ...reactRecommended,
       name: 'react',
       files: files,
-      settings: {
-        react: {
-          version,
-        },
-      },
-      rules: {
-        ...reactRecommended.rules,
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-      },
     },
 
     {
