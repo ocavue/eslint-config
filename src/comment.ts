21bbda40142012bc210e9a2a7fa4119b42b9d9a9
ocavue <ocavue@gmail.com>
2026-03-09 23:23:23 +1100

    fix: disable rule `eslint-comments/disable-enable-pair`

--- src/comment.ts
+++ src/comment.ts
@@ -1,7 +1,27 @@
 import { recommended } from '@eslint-community/eslint-plugin-eslint-comments/configs'
 
-import type { Config } from './types.js'
+import type { Config, Rules } from './types.js'
+
+// @keep-sorted
+export const commentRules: Rules = {
+  // https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/no-aggregating-enable.html
+  '@eslint-community/eslint-comments/no-aggregating-enable': 'error',
+
+  // https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/no-duplicate-disable.html
+  '@eslint-community/eslint-comments/no-duplicate-disable': 'error',
+
+  // https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/no-unlimited-disable.html
+  '@eslint-community/eslint-comments/no-unlimited-disable': 'error',
+
+  // https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/no-unused-enable.html
+  '@eslint-community/eslint-comments/no-unused-enable': 'error',
+}
 
 export function comment(): Config[] {
-  return [recommended]
+  return [
+    {
+      ...recommended,
+      rules: commentRules,
+    },
+  ]
 }
