6c61b27fcaf247b3926b9bb057fb81cbac451b7f
ocavue <ocavue@gmail.com>
2026-04-10 13:50:09 +1000

    lint

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,4 @@
-export const GLOB_SRC_EXT = '?([cm])[jt]s?(x)'
+const GLOB_SRC_EXT = '?([cm])[jt]s?(x)'
 export const GLOB_SRC = '**/*.' + GLOB_SRC_EXT
 
 export const GLOB_JS = '**/*.?([cm])js'
@@ -9,35 +9,11 @@ export const GLOB_TSX = '**/*.?([cm])tsx'
 
 export const GLOB_GEN = '**/*.gen.' + GLOB_SRC_EXT
 
-export const GLOB_TEST = '**/*.(spec|test).' + GLOB_SRC_EXT
-
-export const GLOB_STYLE = '**/*.{c,le,sc}ss'
-export const GLOB_CSS = '**/*.css'
-export const GLOB_LESS = '**/*.less'
-export const GLOB_SCSS = '**/*.scss'
-
-export const GLOB_JSON = '**/*.json'
-export const GLOB_JSON5 = '**/*.json5'
-export const GLOB_JSONC = '**/*.jsonc'
-
 export const GLOB_MARKDOWN = '**/*.md'
 export const GLOB_VUE = '**/*.vue'
-export const GLOB_YAML = '**/*.y?(a)ml'
-export const GLOB_HTML = '**/*.htm?(l)'
 
-export const GLOB_ALL_SRC = [
-  GLOB_SRC,
-  GLOB_STYLE,
-  GLOB_JSON,
-  GLOB_JSON5,
-  GLOB_MARKDOWN,
-  GLOB_VUE,
-  GLOB_YAML,
-  GLOB_HTML,
-] as const
-
-export const GLOB_NODE_MODULES = '**/node_modules' as const
-export const GLOB_LOCKFILE = [
+const GLOB_NODE_MODULES = '**/node_modules' as const
+const GLOB_LOCKFILE = [
   '**/package-lock.json',
   '**/yarn.lock',
   '**/pnpm-lock.yaml',
@@ -55,11 +31,6 @@ export const GLOB_EXCLUDE = [
   '**/__snapshots__',
   '**/.tsup',
 ] as const
-
-export const EXTENSIONS = ['ts', 'js']
-  .flatMap((ext) => [ext, ext + 'x'])
-  .flatMap((ext) => [ext, 'm' + ext, 'c' + ext])
-  .flatMap((ext) => [ext, 'd.' + ext])
 
 export function findConfigByName<T extends { name?: string }>(
   configs: T[],
