b4e935fe7354bc843c5b000bc2bd2aca065d2b28
ocavue <ocavue@users.noreply.github.com>
2026-07-06 11:50:47 +1000

    feat: update eslint-plugin-unicorn to v70 (#488)

--- src/unicorn.ts
+++ src/unicorn.ts
@@ -111,6 +111,9 @@ export const unicornRules: Linter.RulesRecord = {
   // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v66.0.0/docs/rules/no-subtraction-comparison.md
   'unicorn/no-subtraction-comparison': 'warn',
 
+  // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v70.0.0/docs/rules/no-unnecessary-array-flat-map.md
+  'unicorn/no-unnecessary-array-flat-map': 'warn',
+
   // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v66.0.0/docs/rules/no-unnecessary-global-this.md
   'unicorn/no-unnecessary-global-this': 'warn',
 
@@ -179,6 +182,9 @@ export const unicornRules: Linter.RulesRecord = {
 
   // Prefer BigInt literals over the constructor
   'unicorn/prefer-bigint-literals': 'warn',
+
+  // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v70.0.0/docs/rules/prefer-block-statement-over-iife.md
+  'unicorn/prefer-block-statement-over-iife': 'warn',
 
   // Prefer Date.now() to get the number of milliseconds since the Unix Epoch
   // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/v62.0.0/docs/rules/prefer-date-now.md
