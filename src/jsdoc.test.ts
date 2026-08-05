d465c4617b2fb5e10aa0469adfdb4eb4c8db6889
ocavue <ocavue@users.noreply.github.com>
2026-08-05 11:43:02 +1000

    fix: tweak jsdoc rules (#508)

--- src/jsdoc.test.ts
+++ src/jsdoc.test.ts
@@ -7,6 +7,12 @@ import { checkRules } from './test-utils.js'
 test('JSDoc rules should match recommended rules', () => {
   // @keep-sorted
   const disabledRules = [
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/check-alignment.md
+    'jsdoc/check-alignment',
+
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/escape-inline-tags.md
+    'jsdoc/escape-inline-tags',
+
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/implements-on-classes.md
     'jsdoc/implements-on-classes',
 
@@ -18,6 +24,9 @@ test('JSDoc rules should match recommended rules', () => {
 
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-jsdoc.md
     'jsdoc/require-jsdoc',
+
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-next-type.md
+    'jsdoc/require-next-type',
 
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-param-description.md
     'jsdoc/require-param-description',
@@ -40,11 +49,20 @@ test('JSDoc rules should match recommended rules', () => {
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-returns.md
     'jsdoc/require-returns',
 
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-yields-check.md
+    'jsdoc/require-yields-check',
+
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-yields-type.md
+    'jsdoc/require-yields-type',
+
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/require-yields.md
     'jsdoc/require-yields',
 
     // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/tag-lines.md
     'jsdoc/tag-lines',
+
+    // https://github.com/gajus/eslint-plugin-jsdoc/blob/v63.3.3/docs/rules/ts-no-empty-object-type.md
+    'jsdoc/ts-no-empty-object-type',
   ]
 
   checkRules({
